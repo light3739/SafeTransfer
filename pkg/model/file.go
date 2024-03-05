4180c59ba52cc8920d444924b614ef7150e271b3
light3739 <maxikpetlitskii@gmail.com>
2024-03-05 18:24:04 +0100

    Fix column naming

--- pkg/model/file.go
+++ pkg/model/file.go
@@ -6,8 +6,8 @@ import "gorm.io/gorm"
 
 type File struct {
 	gorm.Model
-	CID           string `gorm:"type:varchar(255);unique_index"`
+	CID           string `gorm:"column:cid;type:varchar(255);unique_index"`
 	EncryptionKey string `gorm:"type:varchar(255)"`
 	Nonce         string `gorm:"type:varchar(255)"`
-	Signature     string `gorm:"type:text"` // Add this line
+	Signature     string `gorm:"type:text"`
 }
