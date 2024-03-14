cf5301c48a3e63d0a7acc848b09807618e03817f
Maksim <maxikpetlitskii@gmail.com>
2024-03-14 21:40:56 +0100

    Adding user_repository.go and user_service.go for auth with MetaMask .

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,1 +1,37 @@
+// internal/repository/user_repository_impl.go
+
 package repository
+
+import (
+	"SafeTransfer/internal/db"
+	"SafeTransfer/internal/model"
+	"errors"
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	SaveOrUpdateUser(user *model.User) error
+}
+
+type UserRepositoryImpl struct {
+	DB *gorm.DB
+}
+
+func NewUserRepository(db *db.Database) UserRepository {
+	return &UserRepositoryImpl{DB: db.DB}
+}
+
+func (repo *UserRepositoryImpl) SaveOrUpdateUser(user *model.User) error {
+	var existingUser model.User
+	result := repo.DB.Where("ethereum_address = ?", user.EthereumAddress).First(&existingUser)
+
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return repo.DB.Create(user).Error
+	} else if result.Error != nil {
+		// An unexpected error occurred
+		return result.Error
+	}
+
+	existingUser.Nonce = user.Nonce
+	return repo.DB.Save(&existingUser).Error
+}
--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,1 +1,37 @@
+// internal/service/user_service.go
+
 package service
+
+import (
+	"SafeTransfer/internal/model"
+	"SafeTransfer/internal/repository"
+	"crypto/rand"
+	"encoding/hex"
+)
+
+type UserService struct {
+	UserRepo repository.UserRepository
+}
+
+func NewUserService(userRepo repository.UserRepository) *UserService {
+	return &UserService{
+		UserRepo: userRepo,
+	}
+}
+
+// GenerateNonceForUser generates a unique nonce for the user and stores it in the database.
+func (us *UserService) GenerateNonceForUser(ethereumAddress string) (string, error) {
+	nonceBytes := make([]byte, 16) // 128-bit nonce
+	if _, err := rand.Read(nonceBytes); err != nil {
+		return "", err
+	}
+	nonce := hex.EncodeToString(nonceBytes)
+
+	// Store or update the nonce for the user
+	user := &model.User{
+		EthereumAddress: ethereumAddress,
+		Nonce:           nonce,
+	}
+	err := us.UserRepo.SaveOrUpdateUser(user)
+	return nonce, err
+}
