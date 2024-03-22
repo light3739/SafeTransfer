dc9efa70ec49590ee912809430f7459ce1725e85
Maksim <maxikpetlitskii@gmail.com>
2024-03-22 14:47:32 +0100

    Small bug fixing , refactoring , using Gosec

--- internal/api/response.go
+++ internal/api/response.go
@@ -16,14 +16,20 @@ func RespondWithError(w http.ResponseWriter, code int, message string) {
 // RespondWithJSON sends a JSON response with the provided data.
 func RespondWithJSON(w http.ResponseWriter, status int, data interface{}) {
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(data)
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		http.Error(w, "Failed to encode JSON response", http.StatusInternalServerError)
+		return
+	}
 }
 
 // SendFile sends the file data in the response.
 func SendFile(w http.ResponseWriter, reader io.Reader, filename string) {
 	w.Header().Set("Content-Disposition", "attachment; filename="+filename)
 	w.Header().Set("Content-Type", "application/octet-stream")
-
-	// Write the file data to the response
-	io.Copy(w, reader)
+	_, err := io.Copy(w, reader)
+	if err != nil {
+		http.Error(w, "Failed to send file", http.StatusInternalServerError)
+		return
+	}
 }
